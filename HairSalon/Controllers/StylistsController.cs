568ef16299caa1523a04ecaf94e678000c71d10a
Abel Trotter <a_trotter0@hotmail.com>
2018-07-20 09:29:02 -0700

    Add index view and route in Stylist controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistsController : Controller
+    {
+        private HairSalonContext db = new HairSalonContext();
+
+        [HttpGet("/stylists")]
+        public ActionResult Index()
+        {
+            return View(db.Stylists.ToList());
+        }
+    }
+}
